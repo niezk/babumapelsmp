a51cfdd96a47bdb7d0702ea5649cf705e1501b51
naninuneno <mhafidzarrahman4@gmail.com>
2023-01-23 11:13:18 +0700

    bisa diganti coyyy

---
+++ Private/test.js
@@ -0,0 +1,11 @@
+
+// make value to array
+const turntoarray = "ABCDEFGHIJKLMN"
+
+console.log(Array.from(turntoarray)) // Array.from( var or value )
+
+console.log("Index first from turntoarray is " + turntoarray[0])
+
+let d = new Date()
+d.setDate(d.getDate() + 1);
+console.log("besok" + d)
--- src/script.js
+++ src/script.js
@@ -12,11 +12,11 @@ document.getElementById("tanggal").innerHTML = day[hari] + " - " + tanggal + " -
 
 
 
-const Senin = ["MTK", "MTK", "MTK", "MTK", "MTK",]
-const Selasa = ["MTK", "MTK", "MTK", "MTK", "MTK",]
-const Rabu = ["MTK", "MTK", "MTK", "MTK", "MTK",]
-const Kamis = ["MTK", "MTK", "MTK", "MTK", "MTK",]
-const Jumat = ["MTK", "MTK", "MTK", "MTK", "MTK",]
+const Senin = ["B. Inggris", "B. Indonesia", "BK", "SBK",]
+const Selasa = ["MTK", "IPA", "IPS", "B. Indonesia",]
+const Rabu = ["B. Jawa", "Prakarya", "B. Inggris", "IPA", "MTK",]
+const Kamis = ["PJOK", "PPKN", "BK", "B. indo",]
+const Jumat = ["JB (jamkos:v)", "IPS", "PAI",]
 const Sabtu = ["LIBUR BANH"]
 const Minggu = ["LIBUR BANH"]
 
@@ -26,15 +26,163 @@ const Minggu = ["LIBUR BANH"]
 var mapeltabel = document.getElementById("pelajaran")
 var harinya = document.getElementById("hari")
 
-var besok = Dates.setDate(Dates.getDate() + 1);
-console.log(besok)
-let isi = ""
 
+let isi = ""
+const refreshbut = document.querySelector("#refresh");
+var a = false
+refreshbut.addEventListener('click', function(){
+    if (a === true ) {
+        a = false;
+        mapelawal()
+    } else {
+        a = true;
+        mapelchange()
+    }
+    console.log(a)
+})
+
+
+  
+function mapelchange() {
+    document.getElementById("mapel-kapan").innerHTML = "MAPEL HARI INI"
+    isi = ""
+
+    // MINGGU
+    if (hari === 0) {
+        harinya.innerHTML = day[hari]
+        Minggu.forEach(blablafunction)
+    } 
+
+    // SENIN 
+    if (hari === 1) {
+        harinya.innerHTML = day[hari]
+        Senin.forEach(blablafunction)
+    }
+
+    // SELASA 
+    if (hari === 2) {
+        harinya.innerHTML = day[hari]
+        Selasa.forEach(blablafunction)
+    }
+
+    // RABU 
+    if (hari === 3) {
+        harinya.innerHTML = day[hari]
+        Rabu.forEach(blablafunction)
+    }
+
+    // KAMIS 
+    if (hari === 4) {
+        harinya.innerHTML = day[hari]
+        Kamis.forEach(blablafunction)
+    }
+
+    // JUMAT 
+    if (hari === 5) {
+        harinya.innerHTML = day[hari]
+        Jumat.forEach(blablafunction)
+    }
+
+    // SABTU 
+    if (hari === 6) {
+        harinya.innerHTML = day[hari]
+        Sabtu.forEach(blablafunction)
+    }
+    mapeltabel.innerHTML = isi
+}
+
+function mapelawal() {
+    document.getElementById("mapel-kapan").innerHTML = "MAPEL BESOK"
+    isi = ""
+            // MINGGU
+        if (hari === 0) {
+            harinya.innerHTML = day[hari + 1]
+            Senin.forEach(blablafunction)
+        } 
+
+        // SENIN 
+        if (hari === 1) {
+            harinya.innerHTML = day[hari + 1]
+            Selasa.forEach(blablafunction)
+        }
+
+        // SELASA 
+        if (hari === 2) {
+            harinya.innerHTML = day[hari + 1]
+            Rabu.forEach(blablafunction)
+        }
+
+        // RABU 
+        if (hari === 3) {
+            harinya.innerHTML = day[hari + 1]
+            Kamis.forEach(blablafunction)
+        }
+
+        // KAMIS 
+        if (hari === 4) {
+            harinya.innerHTML = day[hari + 1]
+            Jumat.forEach(blablafunction)
+        }
+
+        // JUMAT 
+        if (hari === 5) {
+            harinya.innerHTML = day[hari + 1]
+            Sabtu.forEach(blablafunction)
+        }
+
+        // SABTU 
+        if (hari === 6) {
+            harinya.innerHTML = day[hari + 1]
+            Minggu.forEach(blablafunction)
+        }
+    
+    mapeltabel.innerHTML = isi;
+}
+
+// MINGGU
 if (hari === 0) {
-    harinya.innerHTML = day[hari]
+    harinya.innerHTML = day[hari + 1]
+    Senin.forEach(blablafunction)
+} 
+
+// SENIN 
+if (hari === 1) {
+    harinya.innerHTML = day[hari + 1]
+    Selasa.forEach(blablafunction)
+}
+
+// SELASA 
+if (hari === 2) {
+    harinya.innerHTML = day[hari + 1]
+    Rabu.forEach(blablafunction)
+}
+
+// RABU 
+if (hari === 3) {
+    harinya.innerHTML = day[hari + 1]
+    Kamis.forEach(blablafunction)
+}
+
+// KAMIS 
+if (hari === 4) {
+    harinya.innerHTML = day[hari + 1]
+    Jumat.forEach(blablafunction)
+}
+
+// JUMAT 
+if (hari === 5) {
+    harinya.innerHTML = day[hari + 1]
+    Sabtu.forEach(blablafunction)
+}
+
+// SABTU 
+if (hari === 6) {
+    harinya.innerHTML = day[hari + 1]
     Minggu.forEach(blablafunction)
 }
+
 mapeltabel.innerHTML = isi
 function blablafunction(value, index, array) {
-   isi += value + "<br>"
+   isi += value + "<br>" + "<br>"
 }
+
